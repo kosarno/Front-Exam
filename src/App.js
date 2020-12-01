825099b9a1b8b64544efe2c7b6f9a93de86d6088
behi <behnamshojaei77@gmail.com>
2020-12-01 16:52:58 +0330

    student Panel design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import logo from './logo.svg';
 import React, { PureComponent } from 'react'
+import Clock from 'react-clock'
 
 import {Layout, Divider, Col, Row} from 'antd'
 import './App.css';
-import Header from './Component/Header'
-import Aside from './Component/Aside'
-import Content from './Component/Content'
+import Header from './Shared/Header/Header'
+import Aside from './Shared/Sidebar/Aside'
+import Content from './Shared/Content'
+import Title from './Shared/ContentTitle/Title'
+import classes from './Shared/Sidebar/Aside.module.css'
+
 
 function App() {
+  // const [SideBarOpen, setSideBarOpen] = useState(true)
+
+
+  // toggle=()=>{
+  //   setIsLoginMode(prevMode => !prevMode);
+  // }
+  // let sidebarClassname = sidebarOpen ? 'sidebar open' : 'sidebar';
+  // let contentClassname = sidebarOpen ? 'content open' : 'content';
+
   return (
     <React.Fragment>
       
       <Layout className="container" style={{height:'100vh'}}>
       <Aside></Aside>
-      <Content></Content>
-        
+      <Layout className="site-layout">  
+        <Header></Header>
+        <Title/>
+        <Content></Content>
+        </Layout>
+          
       </Layout>
       
     </React.Fragment>
